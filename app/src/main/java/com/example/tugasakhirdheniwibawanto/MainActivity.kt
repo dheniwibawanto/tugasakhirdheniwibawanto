12fdc1e3f255c26be7723ad960bc3a6782610aa0
dheniwibawanto <sixsoul@gmail.com>
2025-06-04 23:27:59 +0700

    Pesan commit terbaru

--- app/src/main/java/com/example/tugasakhirdheniwibawanto/MainActivity.kt
+++ app/src/main/java/com/example/tugasakhirdheniwibawanto/MainActivity.kt
@@ -1,162 +1,343 @@
-    package com.example.tugasakhirdheniwibawanto
+package com.example.tugasakhirdheniwibawanto
 
-    import android.os.Bundle
-    import androidx.activity.ComponentActivity
-    import androidx.activity.compose.setContent
-    import androidx.activity.enableEdgeToEdge
-    import androidx.annotation.DrawableRes
-    import androidx.annotation.StringRes
-    import androidx.compose.foundation.Image
-    import androidx.compose.foundation.layout.Column
-    import androidx.compose.foundation.layout.Row
-    import androidx.compose.foundation.layout.fillMaxSize
-    import androidx.compose.foundation.layout.fillMaxWidth
-    import androidx.compose.foundation.layout.heightIn
-    import androidx.compose.foundation.layout.padding
-    import androidx.compose.foundation.layout.paddingFromBaseline
-    import androidx.compose.foundation.layout.size
-    import androidx.compose.foundation.layout.width
-    import androidx.compose.foundation.shape.CircleShape
-    import androidx.compose.foundation.shape.CornerBasedShape
-    import androidx.compose.material.icons.Icons
-    import androidx.compose.material.icons.filled.Search
-    import androidx.compose.material3.Icon
-    import androidx.compose.material3.MaterialTheme
-    import androidx.compose.material3.Scaffold
-    import androidx.compose.material3.Surface
-    import androidx.compose.material3.Text
-    import androidx.compose.material3.TextField
-    import androidx.compose.material3.TextFieldDefaults
-    import androidx.compose.runtime.Composable
-    import androidx.compose.ui.Alignment
-    import androidx.compose.ui.Modifier
-    import androidx.compose.ui.draw.clip
-    import androidx.compose.ui.layout.ContentScale
-    import androidx.compose.ui.res.painterResource
-    import androidx.compose.ui.res.stringResource
-    import androidx.compose.ui.tooling.preview.Preview
-    import androidx.compose.ui.unit.dp
-    import com.example.tugasakhirdheniwibawanto.ui.theme.TugasAkhirDheniWibawantoTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tugasakhirdheniwibawanto.ui.theme.TugasAkhirDheniWibawantoTheme
 
-    class MainActivity : ComponentActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
             setContent {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    SearchBar()
-                    AlignYourBodyElement(
-                        text = R.string.kuliner,
-                        drawable = R.drawable.kuliner,
-                        modifier = Modifier.padding(top = 16.dp)
-                    )
-                }
+                val windowSizeClass = calculateWindowSizeClass(this)
+                MyHealthApp(windowSizeClass)
             }
         }
     }
+}
 
-    @Composable
-    fun SearchBar(
-        modifier: Modifier = Modifier
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextField(
-            value = "",
-            onValueChange = {},
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = null
-                )
-            },
-            colors = TextFieldDefaults.colors(
-                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
-                focusedContainerColor = MaterialTheme.colorScheme.surface
-            ),
-            placeholder = {
-                Text(stringResource(R.string.placeholder_search))
-            },
-            modifier = modifier
-                .fillMaxWidth()
-                .heightIn(min = 56.dp)
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(text),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
         )
     }
+}
 
-    @Composable
-    fun AlignYourBodyElement(
-        @DrawableRes drawable: Int,
-        @StringRes text: Int,
-        modifier: Modifier = Modifier
+@Composable
+fun FavoriteCollectionCard(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
     ) {
-        Column(
-            modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(255.dp)
         ) {
             Image(
                 painter = painterResource(drawable),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(88.dp)
-                    .clip(CircleShape)
+                modifier = Modifier.size(80.dp)
             )
             Text(
                 text = stringResource(text),
-                modifier = Modifier.paddingFromBaseline(top = 24.dp,
-                    bottom = 8.dp),
-                style = MaterialTheme.typography.bodyMedium
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp)
             )
         }
     }
+}
+
+// Data Class
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
 
 
-    @Composable
-    fun FavoriteCollectionCard(
-        @DrawableRes drawable: Int,
-        @StringRes text: Int,
-        modifier: Modifier = Modifier
+private val alignYourBodyData = listOf(
+    R.drawable.bakso to R.string.bakso,
+    R.drawable.salad to R.string.salad,
+    R.drawable.soto to R.string.soto,
+    R.drawable.lalapan to R.string.lalapan,
+    R.drawable.lumpia to R.string.lumpia,
+    R.drawable.batagor to R.string.batagor
+).map { DrawableStringPair(it.first, it.second) }
+
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.kumpulankuliner to R.string.kumpulankuliner,
+    R.drawable.kuliner to R.string.kuliner,
+    R.drawable.batagor to R.string.batagor,
+    R.drawable.lumpia to R.string.lumpia,
+    R.drawable.soto to R.string.soto,
+    R.drawable.salad to R.string.salad
+).map { DrawableStringPair(it.first, it.second) }
+
+@Composable
+fun AlignYourBodyRow(modifier: Modifier = Modifier) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
     ) {
-        Surface(
-            shape = MaterialTheme.shapes.medium,
-            color = MaterialTheme.colorScheme.surfaceVariant,
-            modifier = modifier
+        items(alignYourBodyData) { item ->
+            AlignYourBodyElement(item.drawable, item.text)
+        }
+    }
+}
+
+@Composable
+fun FavoriteCollectionsGrid(modifier: Modifier = Modifier) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.height(168.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(item.drawable, item.text)
+        }
+    }
+}
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            text = stringResource(title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.kumpulankuliner) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.Makanan_Favorit) {
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun BottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(R.string.bottom_navigation_home))
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+
+            },
+            label = {
+                Text(stringResource(R.string.bottom_navigation_profile))
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+private fun SampingNavigationRail(modifier: Modifier = Modifier) {
+    NavigationRail(
+        modifier = modifier.padding(start = 8.dp, end = 8.dp),
+        containerColor = MaterialTheme.colorScheme.background,
+    ) {
+
+        Column(
+            modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_home))
+                },
+                selected = true,
+                onClick = {}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_profile))
+                },
+                selected = false,
+                onClick = {}
+            )
+        }
+    }
+}
 
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.width(255.dp)
-            ) {
-                Image(
-                    painter = painterResource(drawable),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(80.dp)
-                )
-                Text(
-                    text = stringResource(text),
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
+@Composable
+fun MyHealthApp(windowSize: WindowSizeClass) {
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            AppPreview()
+        }
+        WindowWidthSizeClass.Medium -> {
+            AppLandscape()
+        }
+        WindowWidthSizeClass.Expanded -> {
+            AppLandscape()
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun AppPreview() {
+    TugasAkhirDheniWibawantoTheme {
+        Scaffold(
+            bottomBar = {
+                BottomNavigation() }
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
+
+@Preview(device = Devices.AUTOMOTIVE_1024p, widthDp = 720,
+    heightDp = 360)
+@Composable
+fun AppLandscape() {
+    TugasAkhirDheniWibawantoTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Row {
+                SampingNavigationRail()
+                HomeScreen()
             }
         }
     }
-
-    @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
-    @Composable
-    fun AppPreview() {
-        TugasAkhirDheniWibawantoTheme() {
-            Column(
-            ) {
-                SearchBar(Modifier.padding(8.dp))
-                AlignYourBodyElement(
-                    text = R.string.kuliner,
-                    drawable = R.drawable.kuliner,
-                    modifier = Modifier.padding(8.dp)
-                )
-                FavoriteCollectionCard(
-                    text = R.string.kumpulankuliner,
-                    drawable = R.drawable.kumpulankuliner,
-                    modifier = Modifier.padding(8.dp)
-                )
-            }
-        }
-    }
+}
